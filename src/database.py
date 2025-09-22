76cee1c0690e1d93218c57bef7b5bfced85cc439
alexey <aleksei.bakeev@strana.com>
2025-09-23 01:14:25 +0300

    fix(src): Adding migration for model coordinate and image

--- src/database.py
+++ src/database.py
@@ -8,8 +8,5 @@ DATABASE_PARAMS = {}
 
 engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
 
-# async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
-
 class Base(DeclarativeBase):
     pass
---
+++ src/models/coordinate.py
@@ -0,0 +1,20 @@
+import uuid
+
+from sqlalchemy import Column, String, Text, UUID, ForeignKey
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class CoordinatePoint(Base):
+    """Координаты и данные точки."""
+    __tablename__ = "coordinate_points"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    latitude = Column(String(50))
+    longitude = Column(String(50))
+    message = Column(Text)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id", ondelete='CASCADE'), nullable=False)
+    image_id = Column(UUID(as_uuid=True), ForeignKey("image_points.id", ondelete='CASCADE'))
+
+    user = relationship('User', back_populates='points')
+    image = relationship('ImagePoint', back_populates='points')
